1974fd35ce52416c43e0c12d92c421d0f33df8d5
luccas222 <luccas222@gmail.com>
2023-03-29 19:07:10 -0600

    deleted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     private: {
-      weatherKey: "f14225ebd8e6cc58be64228d815bbbeb",
-      imageKey: "9aN24BXnv-LqHHUiWV3IG8UcUWDopgVqOjKUn8LT1lU",
+      weatherKey: "",
+      imageKey: "",
     },
   },
 });
